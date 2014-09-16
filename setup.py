faf2a3b11a4df4e23c9032f038a9d2ee6a3e8679
Robin Winslow <robin@robinwinslow.co.uk>
2014-09-16 12:22:45 +0100

    Improvements to setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='django-asset-server-url',
     version='0.1',
     author='Robin',
-    author_email='robin@robinwinslow.co.uk',
+    author_email='robin.winslow@canonical.com',
+    url='https://github.com/ubuntudesign/django-asset-server-url',
     packages=[
         'django_asset_server_url'
     ],
     description=(
         'Provides the URL for an asset server, as a template context variable.'
     ),
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     install_requires=[
         "Django >= 1.3",
     ],
